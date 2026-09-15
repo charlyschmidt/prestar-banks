4f478bd8f6896f3b1eb5c4b6cf091f477595fe29
cschmidt <cschmidt@latcom.com>
2026-09-15 11:37:27 -0300

    Orden Bancos + cuenta virtual

--- app/Support/ArgentineBanks.php
+++ app/Support/ArgentineBanks.php
@@ -5,41 +5,77 @@ namespace App\Support;
 class ArgentineBanks
 {
     public const BANKS = [
-        'Banco de la Nación Argentina',
-        'Banco de la Provincia de Buenos Aires',
-        'Banco de la Ciudad de Buenos Aires',
-        'Banco Santander Argentina',
-        'Banco Galicia',
-        'BBVA Argentina',
-        'Banco Macro',
-        'Banco Credicoop',
-        'ICBC Argentina',
-        'Banco Patagonia',
-        'Banco Supervielle',
-        'Banco Hipotecario',
-        'Banco Comafi',
-        'Banco de Corrientes',
-        'Banco de Córdoba',
-        'Banco de Entre Ríos',
-        'Banco de Formosa',
-        'Banco de La Pampa',
-        'Banco de San Juan',
-        'Banco de Santa Cruz',
-        'Banco de Santa Fe',
-        'Banco del Chubut',
-        'Banco del Sol',
-        'Banco Dino',
-        'Banco Julio',
-        'Banco Mariva',
-        'Banco Meridian',
-        'Banco Municipal de Rosario',
-        'Banco Piano',
-        'Banco Roela',
-        'Banco Sáenz',
-        'Brubank',
-        'Reba',
-    ];
 
+        'Banco Comafi',
+
+        'Banco Credicoop',
+
+        'Banco de Córdoba',
+
+        'Banco de Corrientes',
+
+        'Banco de Entre Ríos',
+
+        'Banco de Formosa',
+
+        'Banco de La Pampa',
+
+        'Banco de la Ciudad de Buenos Aires',
+
+        'Banco de la Nación Argentina',
+
+        'Banco de la Provincia de Buenos Aires',
+
+        'Banco de San Juan',
+
+        'Banco de Santa Cruz',
+
+        'Banco de Santa Fe',
+
+        'Banco del Chubut',
+
+        'Banco del Sol',
+
+        'Banco Dino',
+
+        'Banco Galicia',
+
+        'Banco Hipotecario',
+
+        'Banco Julio',
+
+        'Banco Macro',
+
+        'Banco Mariva',
+
+        'Banco Meridian',
+
+        'Banco Municipal de Rosario',
+
+        'Banco Patagonia',
+
+        'Banco Piano',
+
+        'Banco Roela',
+
+        'Banco Sáenz',
+
+        'Banco Santander Argentina',
+
+        'Banco Supervielle',
+
+        'BBVA Argentina',
+
+        'Brubank',
+
+        'Cuenta Virtual',
+
+        'ICBC Argentina',
+
+        'Reba',
+
+    ];
+    
     public static function all(): array
     {
         return self::BANKS;
